28d2609ed4301f1edf8ab6fdf55895a858afef24
Sebastian Pulak <pulakosso@gmail.com>
2021-09-10 01:16:04 +0200

    handle multiple raids

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,27 @@ export default function Home() {
   const [result2, setResult2] = useState();
   const [result3, setResult3] = useState();
   const [membersArray, setMembersArray] = useState();
+  const [destructionArray, setDestructionArray] = useState();
   const [runesArray, setRunesArray] = useState();
   const [potsArray, setPotsArray] = useState();
   const [healthstoneSeedArr, setHealthstoneSeedArr] = useState();
   const [token, setToken] = useState();
   const [allReports, setAllReports] = useState();
   const [choosenLog, setChoosenLog] = useState();
+  const [reportsArray, setReportsArray] = useState();
+  const [showText, setShowText] = useState(false);
 
   const handleSelect=(e)=>{
+    console.log(allReports);
     setChoosenLog(e);
+    let temp = [];
+    for(let i=0; i < allReports.length; i++){
+      temp.push({code: allReports[i].code, title: allReports[i].title})
+      if (allReports[i].title === e) break;
+    }
+    setReportsArray(temp);
+    console.log(temp);
+
   }
 
   const handleGetGuildReports = async () => {
@@ -81,126 +93,161 @@ export default function Home() {
 
   const handleSubmit = async (event) => {
 
-    let tempCode;
     event.preventDefault();
-    allReports.map((e) => {
-      if(e.title === choosenLog){
-        tempCode = e.code;
-      }
-    })
+
 
     setIsLoading(true);
+    let hastePotArr = [];
+    let destructionPotArr = [];
+    let allMembersArr = [];
+    let runesArr = [];
+    let manaPotsArr = [];
+    let hsSeedsArr = [];
 
-    const hastePot = await fetch('/api/hastePot?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
+      for (let i = 0; i < reportsArray.length; i++){
+        const allMembers = await fetch('/api/reportMembers?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
 
-    const destructionPot = await fetch('/api/destructionPot?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
+        const json = await allMembers.json();
+        let members = json.reportData.report.table.data.entries;
 
-    const allMembers = await fetch('/api/reportMembers?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
+        for(let i = 0;i<members.length; i++){
+          allMembersArr.push({raider: members[i], totalRaids: 0});
+        }
+      }
 
-    const darkRune = await fetch('/api/darkRune?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
-
-    const demonicRune = await fetch('/api/demonicRune?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
-
-    const manaPot = await fetch('/api/manaPot?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
-
-    const healthstone = await fetch('/api/healthstone?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
-
-    const nightmareSeed = await fetch('/api/nightmareSeed?' + new URLSearchParams({
-      code: event.target.name.value ? event.target.name.value : tempCode,
-      token: token
-    }).toString());
-
-    let arr = [];
-    let arr2 = [];
-    let manaPotArr = [];
-    let hsSeedArr = [];
-    const json = await allMembers.json();
-    console.log(json);
-    let members = json.reportData.report.table.data.entries;
-    console.log(members)
-    for(let i = 0;i < members.length; i++){
-      arr.push({ member: members[i], totalHaste: 0, totalDestruction: 0, totalRune: 0});
-      arr2.push({ member: members[i], totalHaste: 0, totalDestruction: 0, totalRune: 0});
-      manaPotArr.push({ member: members[i], totalManaPot: 0});
-      hsSeedArr.push({ member: members[i], totalHealthstoneSeed: 0});
+    for (let i = 0; i < allMembersArr.length; i++) {
+      let counter = 0;
+      for (let j = 0; j < allMembersArr.length; j++) {
+        if(allMembersArr[i].raider.name === allMembersArr[j].raider.name) counter++
+      }
+      allMembersArr[i].totalRaids = counter;
     }
-    console.log(arr);
-    const hastePotJson = await hastePot.json();
-    const destructionPotJson = await destructionPot.json();
-    const demonicRuneJson = await demonicRune.json();
-    const darkRuneJson = await darkRune.json();
-    const manaPotJson = await manaPot.json();
-    const healthstoneJson = await healthstone.json();
-    const nightmareSeedJson = await nightmareSeed.json();
 
-    arr.map((e) =>{
-      hastePotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalHaste = hastePotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total : 0;
-      destructionPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalDestruction = destructionPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total : 0;
-    })
+      const tempMembersArray = allMembersArr.filter((v,i,a)=> a.findIndex(t=>(t.raider.name === v.raider.name))===i);
 
-    arr2.map((e) =>{
-      demonicRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalRune = demonicRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalRune : 0;
-      darkRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalRune = darkRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalRune: 0;
-    })
 
-    manaPotArr.map((e) =>{
-      manaPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalManaPot = manaPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalManaPot : 0;
-    })
+      for(let i = 0;i < tempMembersArray.length; i++){
+        hastePotArr.push({member: tempMembersArray[i].raider, totalHaste: 0, totalRaids: tempMembersArray[i].totalRaids});
+        runesArr.push({member: tempMembersArray[i].raider, totalRune: 0, totalRaids: tempMembersArray[i].totalRaids});
+        destructionPotArr.push({member: tempMembersArray[i].raider, totalDestruction: 0, totalRaids: tempMembersArray[i].totalRaids});
+        manaPotsArr.push({member: tempMembersArray[i].raider, totalManaPot: 0, totalRaids: tempMembersArray[i].totalRaids});
+        hsSeedsArr.push({member: tempMembersArray[i].raider, totalHealthstoneSeed: 0, totalRaids: tempMembersArray[i].totalRaids});
+      }
 
-    hsSeedArr.map((e) =>{
-      healthstoneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalHealthstoneSeed = healthstoneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalHealthstoneSeed : 0;
-      nightmareSeedJson.reportData.report.table.data.entries.find(element => element.name === e.member.name) ? e.totalHealthstoneSeed = nightmareSeedJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalHealthstoneSeed : 0;
-    })
-
-    arr2.sort((firstItem, secondItem) => secondItem.totalRune - firstItem.totalRune);
-    arr.sort((firstItem, secondItem) => secondItem.totalHaste - firstItem.totalHaste);
-    arr.sort((firstItem, secondItem) => secondItem.totalDestruction - firstItem.totalDestruction);
-    manaPotArr.sort((firstItem, secondItem) => secondItem.totalManaPot - firstItem.totalManaPot);
-    hsSeedArr.sort((firstItem, secondItem) => secondItem.totalHealthstoneSeed - firstItem.totalHealthstoneSeed);
+      for (let i = 0; i < reportsArray.length; i++){
+        const hastePot = await fetch('/api/hastePot?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
     
-    setResult(json);
-    setResult2(hastePotJson);
-    setResult3(destructionPotJson);
-    setMembersArray(arr);
-    setRunesArray(arr2);
-    setPotsArray(manaPotArr);
-    setHealthstoneSeedArr(hsSeedArr)
+        const destructionPot = await fetch('/api/destructionPot?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+    
+        const darkRune = await fetch('/api/darkRune?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+    
+        const demonicRune = await fetch('/api/demonicRune?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+    
+        const manaPot = await fetch('/api/manaPot?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+    
+        const healthstone = await fetch('/api/healthstone?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+    
+        const nightmareSeed = await fetch('/api/nightmareSeed?' + new URLSearchParams({
+          code: reportsArray[i].code,
+          token: token
+        }).toString());
+
+        const hastePotJson = await hastePot.json();
+        const destructionPotJson = await destructionPot.json();
+        const demonicRuneJson = await demonicRune.json();
+        const darkRuneJson = await darkRune.json();
+        const manaPotJson = await manaPot.json();
+        const healthstoneJson = await healthstone.json();
+        const nightmareSeedJson = await nightmareSeed.json();
+    
+        
+        hastePotArr.map((e) => {
+          if(hastePotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalHaste = hastePotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalHaste;
+          } 
+        })
+        runesArr.map((e) => {
+          if(demonicRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalRune = demonicRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalRune
+          }
+          if(darkRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalRune = darkRuneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalRune
+          }
+        })
+
+        destructionPotArr.map((e) => {
+          if(destructionPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalDestruction = destructionPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalDestruction;
+          }
+        })
+
+        manaPotsArr.map((e) => {
+          if(manaPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalManaPot = manaPotJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalManaPot
+          }
+        })
+
+        hsSeedsArr.map((e) => {
+          if(healthstoneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalHealthstoneSeed = healthstoneJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalHealthstoneSeed
+          }
+          if(nightmareSeedJson.reportData.report.table.data.entries.find(element => element.name === e.member.name)){
+            e.totalHealthstoneSeed = nightmareSeedJson.reportData.report.table.data.entries.find(element => element.name === e.member.name).total + e.totalHealthstoneSeed
+          }
+        })
+      }
+    runesArr.sort((firstItem, secondItem) => secondItem.totalRune - firstItem.totalRune);
+    hastePotArr.sort((firstItem, secondItem) => secondItem.totalHaste - firstItem.totalHaste);
+    destructionPotArr.sort((firstItem, secondItem) => secondItem.totalDestruction - firstItem.totalDestruction);
+    manaPotsArr.sort((firstItem, secondItem) => secondItem.totalManaPot - firstItem.totalManaPot);
+    hsSeedsArr.sort((firstItem, secondItem) => secondItem.totalHealthstoneSeed - firstItem.totalHealthstoneSeed);
+    setMembersArray(hastePotArr);
+    setDestructionArray(destructionPotArr);
+    setRunesArray(runesArr);
+    setPotsArray(manaPotsArr);
+    setHealthstoneSeedArr(hsSeedsArr);
+    setShowText(true);
     setIsLoading(false);
+    
   }
 
   return  (
     
     <div className={styles.container}>
       <Head>
-        <title>RPGLogs API Sample App</title>
+        <title>Consumable checker</title>
         <meta name="description" content="A sample application to demonstrate usage of the RPGLogs v2 API." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://assets.rpglogs.com/img/warcraft/favicon.png" />
+        <link rel="icon" href="https://s.abcnews.com/images/US/JuvenileSeal_1618395587590_hpMain_16x9_992.jpg" />
         
       </Head>
-      {result2 && result && result3 ?
-        <h1>Consumables used {}</h1> : <></>}
+      {membersArray ?
+      <div className={styles.centerText}>
+        <h1>Consumables used in {reportsArray.length} raids: {reportsArray.map((e)=> <a href={`https://classic.warcraftlogs.com/reports/${e.code}`} target="_blank">{e.title}, </a>)}</h1> 
+      </div>
+        : <></>
+        }
       <main className={styles.main}>
 
         {membersArray && runesArray && potsArray && healthstoneSeedArr?
@@ -211,7 +258,7 @@ export default function Home() {
                 {
                 membersArray.map((e) =>
                   (e.member.type === "Hunter" || (e.member.type === "Warrior" && e.member?.talents[2]?.guid < 30) || (e.member.type === "Shaman" && e.member?.talents[1]?.guid > 30) || e.member.type === "Rogue" || (e.member.type === "Druid" && e.member?.talents[1]?.guid > 30))
-                  ?<p style={e.totalHaste > 0 ? { color: "green" } : {color: "red"}}><b>{e.member.name}: {e.totalHaste}</b></p> :
+                  ?<p style={e.totalHaste > 0 ? { color: "green" } : {color: "red"}}><b>({e.totalRaids}/{reportsArray.length}) {e.member.name}: {e.totalHaste}</b></p> :
                   <></>
                 )
               }
@@ -222,7 +269,7 @@ export default function Home() {
                 {
                 runesArray.map((e) =>
                   (e.member.type === "Hunter"  || (e.member.type === "Shaman") || (e.member.type === "Druid" && e.member?.talents[2]?.guid > 30) || (e.member.type === "Druid" && e.member?.talents[0]?.guid > 30))
-                  ?<p style={e.totalRune > 0 ? { color: "green" } : {color: "red"}}><b>{e.member.name}: {e.totalRune}</b></p> :
+                  ?<p style={e.totalRune > 0 ? { color: "green" } : {color: "red"}}><b>({e.totalRaids}/{reportsArray.length}) {e.member.name}: {e.totalRune}</b></p> :
                   <></>
                 )
               }
@@ -233,7 +280,7 @@ export default function Home() {
                 {
                 potsArray.map((e) =>
                   (e.member.type !== "Warrior" && e.member.type !== "Rogue")
-                  ?<p style={e.totalManaPot > 0 ? { color: "green" } : {color: "red"}}><b>{e.member.name}: {e.totalManaPot}</b></p> :
+                  ?<p style={e.totalManaPot > 0 ? { color: "green" } : {color: "red"}}><b>({e.totalRaids}/{reportsArray.length}) {e.member.name}: {e.totalManaPot}</b></p> :
                   <></>
                 )
               }
@@ -244,7 +291,7 @@ export default function Home() {
                 {
                 healthstoneSeedArr.map((e) =>
                 (e.totalHealthstoneSeed > 0) ?
-                  <p style={{ color: "green" }}><b>{e.member.name}: {e.totalHealthstoneSeed}</b></p> : <></>
+                  <p style={{ color: "green" }}><b>({e.totalRaids}/{reportsArray.length}) {e.member.name}: {e.totalHealthstoneSeed}</b></p> : <></>
                 )
               }
               </div>
@@ -252,9 +299,9 @@ export default function Home() {
               <div className={styles.column}>
                 <h3>Destruction potion:</h3>
                 {
-                membersArray.map((e) =>
+                destructionArray.map((e) =>
                 ((e.member.type === "Paladin" && e.member?.talents[1]?.guid > 30) || e.member.type === "Warlock" || e.member.type === "Mage" || (e.member.type === "Priest" && e.member?.talents[2]?.guid > 30) || (e.member.type === "Druid" && e.member?.talents[0]?.guid > 30) || (e.member.type === "Shaman" && e.member?.talents[0]?.guid > 30))
-                  ?<p style={e.totalDestruction > 0 ? { color: "green" } : {color: "red"}}><b>{e.member.name}: {e.totalDestruction}</b></p> :
+                  ?<p style={e.totalDestruction > 0 ? { color: "green" } : {color: "red"}}><b>({e.totalRaids}/{reportsArray.length}) {e.member.name}: {e.totalDestruction}</b></p> :
                   <></>
                 )
               }
@@ -285,7 +332,7 @@ export default function Home() {
                   </Dropdown.Menu>
                 </Dropdown>
               </div>
-              <button type="button" className="btn btn-outline-dark" disabled={isLoading}
+              <button type="button" className="btn btn-outline-dark" disabled={isLoading || choosenLog === undefined}
                   type="submit">{isLoading ? 'Loading...' : 'Search!'}</button>
               </form>
             </div>
