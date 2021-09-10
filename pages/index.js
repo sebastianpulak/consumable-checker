0ea8b4adb0d577674c3c6d8ea9279733de36de44
Sebastian Pulak <pulakosso@gmail.com>
2021-09-10 18:35:07 +0200

    forgot paladins too

--- pages/index.js
+++ pages/index.js
@@ -387,7 +387,7 @@ export default function Home() {
                     <h3>Dark runes:</h3>
                     {
                       runesArray.map((e) =>
-                        (e.member.type === "Hunter" || (e.member.type === "Shaman") || (e.member.type === "Druid" && e.member?.talents[2]?.guid > 30) || (e.member.type === "Druid" && e.member?.talents[0]?.guid > 30) || e.member.type === "Priest")
+                        (e.member.type === "Hunter" || (e.member.type === "Shaman") || (e.member.type === "Druid" && e.member?.talents[2]?.guid > 30) || (e.member.type === "Druid" && e.member?.talents[0]?.guid > 30) || e.member.type === "Priest" || e.member.type === "Paladin")
                           ? <p className={styles.text} key={`runesKey${e.member.name}`} style={e.totalRune > 0 ? { color: "green" } : { color: "red" }}><b>{e.member.name}: {e.totalRune} in {e.totalEncounters} || {Math.round((e.totalRune/e.totalEncounters) * 100) / 100}</b></p> :
                           <></>
                       )
